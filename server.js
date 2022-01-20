f2f90861c1f4a01526f9c2801d512c7793ddc12f
AlanNJ <alannpn75@gmail.com>
2022-01-20 13:32:13 +0530

    cors

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.use(express.json({ limit: "5mb" }));
 app.use(urlencoded({ extended: true }));
 app.use(
 	cors({
-		origin: [],
+		origin: ["https://precily-devtem.netlify.app/"],
 	})
 );
 app.use("/api", require("./routes/auth"));
